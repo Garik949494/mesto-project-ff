b456f8aaddd9f3fdad05dda94345ed7d482a892e
Garik <urbandrama@yandex.ru>
2024-07-29 02:38:04 +0300

    передел

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,12 @@ const cardTemplate =  document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placeList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
- function addCard(card){
+ function createCard(card, deleteCallback){
     const cardContent = cardTemplate.querySelector('.card').cloneNode(true);
     cardContent.querySelector('.card__title').textContent = card.name;
     cardContent.querySelector('.card__image').src = card.link;
     const deleteButton = cardContent.querySelector('.card__delete-button');  //присвоили переменной  deleteButton класс кнопки удаления
-    deleteButton.addEventListener('click', () => {cardContent.remove()});        // хочу присвоить переменной кнопки удаления событие удаления по клику через callback
+    deleteButton.addEventListener('click', () => deleteCallback(cardContent));       // хочу присвоить переменной кнопки удаления событие удаления по клику через callback
         
     
     return cardContent;
@@ -22,6 +22,6 @@ const placeList = document.querySelector('.places__list');
 // @todo: Вывести карточки на страницу
 
     initialCards.forEach((element) => {
-    const createCard = addCard(element)
-    placeList.append(createCard);
+    const newCard = createCard(element, cardDelete)
+    placeList.append(newCard);
   })
